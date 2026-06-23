f47d55e4e526bccab5a83920f73b6228735b384b
Biswabhusan Behera <biswabhusanb2@gmail.com>
2026-06-23 12:47:26 +0530

    Delete TravelAgency directory

--- TravelAgency/Travel/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from .models import Package, Booking, ContactMessage
-
-admin.site.register(Package)
-admin.site.register(Booking)
-admin.site.register(ContactMessage)
--- TravelAgency/Travel/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-
-urlpatterns = [
-    path('', views.home,name='home'),
-    path('booking/',views.booking,name='booking')
-    path('about/',views.about,name='about')
-]
